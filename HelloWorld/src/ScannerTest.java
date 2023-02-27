3080ea82539af4b8a657c41de5b246c7d1dca057
codewithxavi <javieradrian.it@gmail.com>
2023-02-27 14:41:45 +0100

    Commit new exercises

---
+++ HelloWorld/src/ScannerTest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+	
+public class ScannerTest {
+
+	public static void main(String[] args) {
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		int numero = keyboard.nextInt();
+	
+		keyboard.close();
+		
+	}
+
+}
